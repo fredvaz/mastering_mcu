60bc05982832644d62823501a1cd3c79f911f75d
fredvaz <fredvaz8@gmail.com>
2023-05-28 02:00:38 +0100

    template added

---
+++ fundamentals/Template/Core/Src/main.c
@@ -0,0 +1,36 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+#include <stdint.h>
+
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  uint32_t x_value = 10;
+
+  while (1)
+  {
+
+  }
+
+}
